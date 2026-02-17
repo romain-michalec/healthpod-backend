a3c880cc605b3b3336277d727139cd0942936d33
Romain Michalec <4263570+romain-michalec@users.noreply.github.com>
2026-02-17 11:51:14 +0000

    Support multiple successive connections on the listener side

--- dummy.py
+++ dummy.py
@@ -1,27 +1,45 @@
 from multiprocessing.connection import Listener
 
-host = "localhost"
-"""Network interface to listen on.
+address = (host, port) = ("localhost", 61000)
+"""Network interface and TCP port to listen on.
 
-Interface name (such as localhost) or IP address (such as 127.0.0.1). If
-using an empty string, the server will listen for incoming connections
-on all available network interfaces.
+The network interface can be a hostname (such as localhost), an IP
+address (such as 127.0.0.1), or an emptry string (to bind the listening
+socket to all the available interfaces).
+
+Choose a port number in the private range and ideally outside the
+operating system's range for ephemeral ports (in /proc/sys/net/ipv4/
+ip_local_port_range).
 """
 
-port = 61000
-"""TCP port to listen on."""
 
-
-# Basic print-to-stdout server
-address = (host, port)
+# This listener prints the messages sent from the other end of the
+# connection to the standard output
 with Listener(address) as listener:
     print(f"Listening for connections on {listener.address}")
 
-    with listener.accept() as connection:  # Block until there is an incoming connection
-        print(f"Connection accepted from {listener.last_accepted}")
+    while True:
+        try:
+            # The next line blocks until there is an incoming connection
+            with listener.accept() as connection:
+                print(f"Connection accepted from {listener.last_accepted}")
 
-        while True:
-            msg = connection.recv()  # Block until there is something to receive
-            print(f"Received: {msg}")
+                while True:
+                    try:
+                        # The next line blocks until there is something to receive
+                        msg = connection.recv()
 
-    print("Connection closed")
+                    except EOFError:
+                        # There is nothing left to receive and the other end was closed
+                        break
+
+                    else:
+                        print(f"Received: {msg}")
+
+            print("Connection closed")
+
+        except KeyboardInterrupt:
+            # The user hit Ctrl+C
+            break
+
+print("Stopped listening")
